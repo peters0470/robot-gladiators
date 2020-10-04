1866b98c35915a957b1ed896c1e7475653d63658
unknown <peters0470@gmail.com>
2020-10-04 14:13:25 -0500

    changes to fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,9 +12,17 @@ var enemyAttack = 12;
 
 
 var fight = function(enemyName) {
-    while(enemyHealth > 0) {
+    while(enemyHealth > 0 && playerHealth > 0) {
 var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-if (promptFight === "fight" || promptFight === "FIGHT") {
+if (promptFight === "skip" || promptFight === "SKIP") {
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+    if (confirmSkip) {
+        window.alert(playerName + " has decided to skip this fight. Goodbye!");
+        playerMoney = playerMoney - 10;
+        console.log("playerMoney" , playerMoney);
+        break;
+    }
+}
   //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
  enemyHealth = enemyHealth - playerAttack;
   // Log a resulting message to the console so we know that it worked.
@@ -23,6 +31,8 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
  // check enemy's health
  if (enemyHealth <= 0) {
      window.alert(enemyName + " has died! ");
+     playerMoney = playerMoney + 20;
+     break;
  }
  else {
      window.alert(enemyName + " still has " + enemyHealth + " health left. ");
@@ -35,23 +45,12 @@ playerHealth = playerHealth - enemyAttack;
   //check players health
   if (playerHealth <= 0) {
       window.alert(playerName + " has died! ");
+      break;
   }
   else {
       window.alert(playerName + " still has " + playerHealth + " health left.");
   }
-} else if (promptFight === "skip" || promptFight === "SKIP") {
-    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-    if (confirmSkip) {
-        window.alert(playerName + " has decided to skip this fight. Goodbye!");
-        playerMoney = playerMoney - 2;
-    }
-    else {
-        fight();
-    } 
-} else {
-    window.alert("You need to pick a valid option. Try again!"); 
 }
-    }
 }; 
 
 for(var i = 0; i < enemyNames.length; i++) {
