15ae4669c911ed001d3e9f37d567be15b8ee5258
unknown <peters0470@gmail.com>
2020-10-04 11:16:25 -0500

    added array loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,11 +6,12 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roberto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+
+var fight = function(enemyName) {
   window.alert("Welcome to Robot Gladiators!");
 var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 if (promptFight === "fight" || promptFight === "FIGHT") {
@@ -50,8 +51,8 @@ playerHealth = playerHealth - enemyAttack;
 } else {
     window.alert("You need to pick a valid option. Try again!"); 
 }
-   
 }; 
 
-
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
