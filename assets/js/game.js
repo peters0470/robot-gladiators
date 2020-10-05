1ae744701f1d4089a24e882b98edbb337cba3657
unknown <peters0470@gmail.com>
2020-10-04 20:56:54 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -18,13 +18,13 @@ if (promptFight === "skip" || promptFight === "SKIP") {
     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
     if (confirmSkip) {
         window.alert(playerName + " has decided to skip this fight. Goodbye!");
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney" , playerMoney);
         break;
     }
 }
-  //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
- enemyHealth = enemyHealth - playerAttack;
+var damage = randomNumber(playerAttack - 3, playerAttack);
+ enemyHealth = Math.max(0, enemyHealth - playerAttack);
   // Log a resulting message to the console so we know that it worked.
   console.log(
  playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
@@ -38,7 +38,8 @@ if (promptFight === "skip" || promptFight === "SKIP") {
      window.alert(enemyName + " still has " + enemyHealth + " health left. ");
  }
   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-playerHealth = playerHealth - enemyAttack;
+  var damage = randomNumber(enemyAttack - 3, enemyAttack);
+playerHealth = Math.max(0, playerHealth - enemyAttack);
   // Log a resulting message to the console so we know that it worked.
   console.log(
   enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."); 
@@ -60,7 +61,7 @@ for (var i = 0; i < enemyNames.length; i++) {
   if (playerHealth > 0) {
       window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
     var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
+    enemyHealth = randomNumber(40, 60);
     fight(pickedEnemyName);
   if (playerHealth > 0 && i < enemyNames.length - 1) {
     var storeConfirm = window.confirm("The fight is over, vist the store before the next round?");
@@ -134,6 +135,11 @@ switch (shopOptionPrompt) {
       // call shop() again to force player to pick a valid option
       shop();
       break;
-  }   
+    }
+  }; 
+var randomNumber = function(min,max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+    return value;
 };
+
 startGame();
